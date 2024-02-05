42b05ded33531028a8a07ca6cb6e34ef19e90ef8
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-05 22:30:26 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
