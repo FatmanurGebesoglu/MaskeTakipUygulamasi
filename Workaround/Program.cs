76a0376caef5c2f6053dbd2ea3b78dcb6f81c368
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-07 12:03:16 +0300

    Person entity created

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -6,8 +6,40 @@ namespace Workaround
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+           // Degiskenler();
+           Vatandas vatandas1 = new Vatandas();
+           
+        }
+
+        private static void Degiskenler()
+        {
+            string mesaj = "merhaba";
+            double tutar = 1000;
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+            string ad = "Fatmanur";
+            string soyad = "Gebeşoğlu";
+            int dogumYili = 2000;
+            long tcNo = 12345678910;
+
+            Console.WriteLine("merhaba");
             Console.ReadLine();
         }
+    }
+
+    public class Vatandas
+    {
+        //public string ad = "Fatmanur";
+        //public string soyad = "Gebeşoğlu";
+        //public int dogumYili = 2000;
+        //public long tcNo = 12345678910;
+
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+
+
     }
 }
